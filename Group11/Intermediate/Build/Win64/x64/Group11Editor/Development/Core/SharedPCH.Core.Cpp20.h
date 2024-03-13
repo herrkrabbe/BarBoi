48152fb8c0e4dc72fe4f07f80c72542944f1eba0
Alexander Linde Lossius <alexander.lossius@gmail.com>
2024-03-13 15:57:07 +0100

    refactor: refactoring github

--- Group11/Intermediate/Build/Win64/x64/Group11Editor/Development/Core/SharedPCH.Core.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "D:/Spillskolen/2024V Programming 2/dev/Unreal/BarBoi/Group11/Intermediate/Build/Win64/x64/Group11Editor/Development/Core/SharedDefinitions.Core.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- Group11/Intermediate/Build/Win64/x64/Group11Editor/Development/Engine/SharedPCH.Engine.Cpp17.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "D:/Spillskolen/2024V Programming 2/dev/Unreal/BarBoi/Group11/Intermediate/Build/Win64/x64/Group11Editor/Development/Engine/SharedDefinitions.Engine.Cpp17.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
