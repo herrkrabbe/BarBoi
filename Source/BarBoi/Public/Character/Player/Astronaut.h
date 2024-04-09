9ea4f14f3bbf1e746b408990fb67e2042df449d7
Alexander Linde Lossius <alexander.lossius@gmail.com>
2024-04-09 11:46:35 +0200

    header: astronaut header input actions

--- Source/BarBoi/Public/Character/Player/Astronaut.h
+++ Source/BarBoi/Public/Character/Player/Astronaut.h
@@ -6,10 +6,43 @@
 #include "GameFramework/Character.h"
 #include "Astronaut.generated.h"
 
+class UInputComponent;
+class USkeletalMeshComponent;
+class UCameraComponent;
+class UInputAction;
+class UInputMappingContext;
+struct FInputActionValue;
+
 UCLASS()
 class BARBOI_API AAstronaut : public ACharacter
 {
 	GENERATED_BODY()
+
+	//Components
+	//Note that Character already includes a SkeletalMeshComponent: Mesh
+
+	/* Weapon for astronaut */
+	UPROPERTY(VisibleDefaultsOnly, Category="Mesh")
+	USkeletalMeshComponent* AstronautWeapon;
+
+	/* Camera for astronaut */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* AstronautCamera;
+
+	/*IMC for astronaut*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Input", meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* AstronautMapppingContext;
+
+	//Input Actions
+
+	/* Action for Actions. Fire, repair etc. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Input", meta = (AllowPrivateAccess = "true"))
+	UInputAction* ActionAction;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Input", meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+
 
 public:
 	// Sets default values for this character's properties
