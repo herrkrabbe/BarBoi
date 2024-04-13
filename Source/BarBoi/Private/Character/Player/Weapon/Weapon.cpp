84e812299d3dedb92cb177ee6b5bbf054ae7e440
Alexander Linde Lossius <alexander.lossius@gmail.com>
2024-04-13 16:27:50 +0200

    feat: many weapon functions implemented

--- Source/BarBoi/Private/Character/Player/Weapon/Weapon.cpp
+++ Source/BarBoi/Private/Character/Player/Weapon/Weapon.cpp
@@ -2,13 +2,39 @@
 
 
 #include "Character/Player/Weapon/Weapon.h"
+#include <EnhancedInputSubsystems.h>
+#include <EnhancedInputComponent.h>
 
 UWeapon::UWeapon()
 {
 }
 
+UWeapon::UWeapon(AAstronaut* character)
+{
+	SetupInput(character);
+
+}
+
+void UWeapon::TickWeapon(float deltaTime)
+{
+	Cooling(deltaTime);
+	FireCooldownTick(deltaTime);
+
+}
+
 void UWeapon::Fire()
 {
+	//TODO
+}
+
+void UWeapon::Repair()
+{
+
+	// weapon cannot repair while overheated or has recently fired
+	if(FireCooldown != 0 || bIsOverheated) return;
+
+
+	//TODO
 }
 
 float UWeapon::GetHeat()
@@ -18,13 +44,95 @@ float UWeapon::GetHeat()
 
 float UWeapon::Cooling(float deltaTime)
 {
-	return 0.0f;
+	//calculating heat removed
+	float heatRemoved = deltaTime * CoolingRate;
+
+	//applying cooling
+	AddHeat(heatRemoved);
+
+	return heatRemoved;
+}
+
+bool UWeapon::SetupInput(AAstronaut* character)
+{
+	if (APlayerController* PlayerController = Cast<APlayerController>(character->GetController()))
+	{
+		Character = character;
+
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			// Set the priority of the mapping to 1, so that it overrides the Jump action with the Fire action when using touch input
+			Subsystem->AddMappingContext(WeaponMappingContext, 3);
+		}
+
+		if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerController->InputComponent))
+		{
+			// Fire
+			EnhancedInputComponent->BindAction(ActionFire, ETriggerEvent::Triggered, this, &UWeapon::Fire);
+			EnhancedInputComponent->BindAction(ActionRepair, ETriggerEvent::Ongoing, this, &UWeapon::Repair);
+		}
+	}
+	return false;
+}
+
+void UWeapon::FireCooldownTick(float deltaTime)
+{
+	if (FireCooldown == 0) return;
+
+	FireCooldown -= deltaTime;
+
+	if (FireCooldown < 0) {
+		FireCooldown = 0;
+	}
 }
 
 void UWeapon::SetHeat(float newHeat)
 {
+	Heat = newHeat;
+
+	ApplyHeatLimits();
 }
 
 void UWeapon::AddHeat(float deltaHeat)
 {
+	//adding heat
+	Heat += deltaHeat;
+
+	//testing for head limits
+	ApplyHeatLimits();
+	
+	
+}
+
+void UWeapon::ApplyHeatLimits()
+{
+	if (Heat <= 0) {
+		//applying floor
+		Heat = 0;
+		bIsOverheated = false;
+		return;
+	}
+
+	if (Heat >= HeatCapacity) {
+		// applying roof
+		Heat = HeatCapacity;
+		bIsOverheated = true;
+		return;
+	}
+}
+
+void UWeapon::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (Character == nullptr)
+	{
+		return;
+	}
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(Character->GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->RemoveMappingContext(WeaponMappingContext);
+		}
+	}
 }
--- Source/BarBoi/Public/Character/Player/Weapon/Weapon.h
+++ Source/BarBoi/Public/Character/Player/Weapon/Weapon.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Components/MeshComponent.h"
+#include <Character/Player/Astronaut.h>
 #include "Weapon.generated.h"
+
 
 /**
  * 
@@ -15,16 +17,25 @@ class BARBOI_API UWeapon : public UMeshComponent
 	GENERATED_BODY()
 	
 public:
-	/* Constructor for weapon*/
+	/* Default Constructor for weapon. If you use this constructor you must then SetupInput*/
 	UWeapon();
 
+	/* Constructor for weapon*/
+	UWeapon(AAstronaut* character);
+
+	/* Function called to tick weapon*/
+	void TickWeapon(float deltaTime);
+
+	/* Projectile class to spawn */
 	UPROPERTY(EditDefaultsOnly, Category = Projectile)
 	TSubclassOf<class ALaser> ProjectileClass;
 
+	/* Input Mapping Context used for the weapon */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	class UInputMappingContext* WeaponMappingContext;
 
-	/** Gun muzzle's offset from the characters location */
+	/** Gun muzzle's offset from the characters location 
+	From FPS Template*/
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
 	FVector MuzzleOffset;
 
@@ -40,18 +51,24 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Weapon")
 	void Fire();
 
+	/* Make weapon repair ship */
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void Repair();
+
 	/* Get Heat value of weapon */
 	UFUNCTION(BlueprintCallable, Category = "Heat")
 	float GetHeat();
 
-	
-
-	/* Function to cool gun. Must be called every tick in parent actor
-	
+	/* Function to cool gun.
 	returns heat removed*/
 	UFUNCTION(BlueprintCallable, Category = "Heat")
 	float Cooling(float deltaTime);
 
+	/* Function called to setup input of weapon*/
+	UFUNCTION(BlueprintCallable, Category = "Input")
+	bool SetupInput(AAstronaut* character);
+
+	
 
 private:
 	/* Float tracking the heat of the weapon. heat starts at 0 */
@@ -64,11 +81,22 @@ private:
 
 	/* Heat removed per second */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Heat", meta = (AllowPrivateAccess = "true"))
-	float CoolingRate = 150;
+	float CoolingRate = -150;
 
 	/* Heat added per shot */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Heat", meta = (AllowPrivateAccess = "true"))
 	float HeatPerShot = 62.5;
+
+	/* Time before weapon can shoot again */
+	UPROPERTY(BlueprintReadWrite, Category = "Fire", meta = (AllowPrivateAccess = "true"))
+	float FireCooldown = 0;
+
+	/* Fire rate of weapon */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fire", meta = (AllowPrivateAccess = "true"))
+	float FireRate = 0.25;
+
+	/* Function to tick down fire cooldown */
+	void FireCooldownTick(float deltaTime);
 
 	/* Bool tracking if the weapon is overheated. starts false */
 	UPROPERTY(BlueprintReadWrite, Category = "Heat", meta = (AllowPrivateAccess = "true"))
@@ -81,5 +109,18 @@ private:
 	/* Add Heat value to weapon */
 	UFUNCTION(BlueprintCallable, Category = "Heat", meta = (AllowPrivateAccess = "true"))
 	void AddHeat(float deltaHeat);
+
+	/* Function to apply heat floor and roof */
+	void ApplyHeatLimits();
+	
+protected:
+	/* Reference to character holding component */
+	AAstronaut* Character;
+
+	/** Ends gameplay for this component.
+	From FPS Template*/
+	UFUNCTION()
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
 
 };
