c244245eeb294c4d012d446c1777b5542ac3fbfd
Alexander Linde Lossius <alexander.lossius@gmail.com>
2024-04-16 13:56:57 +0200

    Astronaut and weapon controls (#25)
    * Added UML.drawio
    
    * alexander working into alexander main (#24)
    
    * feat: laser spawns, and laser class has started coding
    
    * fix: recompiled project

--- Source/BarBoi/Private/Character/Player/Weapon/Laser.cpp
+++ Source/BarBoi/Private/Character/Player/Weapon/Laser.cpp
@@ -6,22 +6,47 @@
 // Sets default values
 ALaser::ALaser()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-// Called when the game starts or when spawned
-void ALaser::BeginPlay()
-{
-	Super::BeginPlay();
 	
+	//enabling ticking for lifespan
+	Super::SetLifeSpan(5.f);
+
+ 	// initialise capsule component used for collision detection
+	CollisionComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CollisionComp"));
+	CollisionComp->InitCapsuleSize(1.f, 4.f);
+
+	//Rotating capsule to be horizontal
+	CollisionComp->AddLocalRotation(FRotator(90.f, 0.f, 0.f));
+
+	//Adding hit event
+	CollisionComp->SetCollisionProfileName("Laser");
+	CollisionComp->OnComponentHit.AddDynamic(this, &ALaser::OnHit);
+
+	//setting capsule as root component
+	RootComponent = CollisionComp;
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+	ProjectileMovement->UpdatedComponent = CollisionComp;
+	ProjectileMovement->InitialSpeed = 3000.f;
+	ProjectileMovement->MaxSpeed = 3000.f;
+	ProjectileMovement->bRotationFollowsVelocity = true;
+	ProjectileMovement->bShouldBounce = false;
+	ProjectileMovement->ProjectileGravityScale = 0.f; // disabling gravity
+	
+	//disable collision of mesh
+	//LaserMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
 }
 
-// Called every frame
-void ALaser::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
 
+
+void ALaser::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if ((OtherActor == nullptr) || (OtherActor == this) || (OtherComp == nullptr)) {
+		return; // return is used here for prettier code
+	}
+
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("TODO: Laser OnHit implementation%f")));
+
+	Destroy();
 }
 
--- Source/BarBoi/Private/Character/Player/Weapon/Weapon.cpp
+++ Source/BarBoi/Private/Character/Player/Weapon/Weapon.cpp
@@ -27,34 +27,71 @@ void UWeapon::TickWeapon(float deltaTime)
 
 void UWeapon::Fire()
 {
+	
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Start Fire%f")));
+
+
 	// This block is taken from FPS template
 	if (Character == nullptr || Character->GetController() == nullptr)
 	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Fail Fire, No Character%f")));
 		return;
 	}
 
-	// weapon cannot fire while overheated or has recently fired
-	if (FireCooldown != 0 || bIsOverheated) return;
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Has Character Character%f")));
 
+	// weapon cannot fire while overheated or has recently fired
+	if (FireCooldown != 0 || bIsOverheated) {
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Fail Fire, Overheated%f")));
+		return;
+	}
 	// Try and fire a projectile. This block is taken from FPS template
 	if (ProjectileClass != nullptr)
 	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Has ProjectileClass%f")));
 		UWorld* const World = GetWorld();
 		if (World != nullptr)
 		{
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Has World%f")));
 			APlayerController* PlayerController = Cast<APlayerController>(Character->GetController());
+			if (PlayerController == nullptr) {
+				GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Fail Fire, No PlayerController%f")));
+				return;
+			}
+			else {
+				GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Has PlayerController%f")));
+			}
 			const FRotator SpawnRotation = PlayerController->PlayerCameraManager->GetCameraRotation();
 			// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
-			const FVector SpawnLocation = GetOwner()->GetActorLocation() + SpawnRotation.RotateVector(MuzzleOffset);
+			const FVector SpawnLocation = GetOwner()->GetActorLocation() + SpawnRotation.RotateVector(MuzzleOffset) + 100.0f;
+
+			FString rotationString = SpawnRotation.ToString();
+			FString locationString = SpawnLocation.ToString();
+
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, rotationString);
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, locationString);
 
 			//Set Spawn Collision Handling Override
 			FActorSpawnParameters ActorSpawnParams;
-			ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+			//ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+			ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 
 			// Spawn the projectile at the muzzle
 			World->SpawnActor<ALaser>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
+
+			// Setting cooldown and adding heat
+			FireCooldown = FireRate;
+			AddHeat(HeatPerShot);
+		}
+		else {
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Fail Fire, No World%f")));
 		}
 	}
+	else {
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Fail Fire, No Projectile Class%f")));
+	}
+
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Fire End%f")));
 }
 
 void UWeapon::Repair()
@@ -110,6 +147,9 @@ void UWeapon::FireCooldownTick(float deltaTime)
 	if (FireCooldown == 0) return;
 
 	FireCooldown -= deltaTime;
+
+	FString cooldownString = FString::Printf(TEXT("Cooldown: %f"), FireCooldown);
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, cooldownString);
 
 	if (FireCooldown < 0) {
 		FireCooldown = 0;
