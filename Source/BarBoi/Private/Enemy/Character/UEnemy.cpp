1b98fa61dc18c8646f708e8584b291e36108975e
ErikEnoksen <269411@stud.inn.no>
2024-05-13 16:19:33 +0200

    feat: fix patrol

--- Source/BarBoi/Private/Enemy/Character/UEnemy.cpp
+++ Source/BarBoi/Private/Enemy/Character/UEnemy.cpp
@@ -134,7 +134,7 @@ void AUEnemy::StopSeekingPlayer()
 void AUEnemy::OnPlayerDetectedOverLapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (GetTarget_Implementation())
+	if (GetTarget_Implementation() == OtherActor)
 	{
 		PlayerDetected = true;
 		SeekPlayer();
@@ -144,7 +144,7 @@ void AUEnemy::OnPlayerDetectedOverLapBegin(UPrimitiveComponent* OverlappedComp, 
 void AUEnemy::OnPlayerDetectedOverLapEnd(UPrimitiveComponent* OverLappedComp, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	if (GetTarget_Implementation())
+	if (GetTarget_Implementation()== OtherActor)
 	{
 		PlayerDetected = false;
 		StopSeekingPlayer();
